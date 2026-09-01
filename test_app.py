fb95f337d2e21496bc21ff9901fb62da75ec991e
kudriavuy <kudriavuy20@gmail.com>
2026-09-01 16:17:45 +0200

    Add automated testing and linting to CI

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client;
+        yield client
+
+def test_hello_route(client):
+    """Перевіряє, чи сервер відповідає статусом 200 та повертає текст."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello" in response.data
