ad6a4556c31236e45c194e789dc9ad9d6e4e87f3
kudriavuy <kudriavuy20@gmail.com>
2026-09-01 16:23:43 +0200

    Fix syntax error in test_app.py

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    with app.test_client() as client;
+    with app.test_client() as client:
         yield client
 
 def test_hello_route(client):
